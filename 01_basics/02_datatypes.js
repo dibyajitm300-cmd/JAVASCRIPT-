68671d62e88e376a4fc992719d66f29befaf9413
dibyajitm300-cmd <dibyajitm300@gmail.com>
2026-03-14 06:45:23 +0000

    datatypes included

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all javascript code as newer version
+// alert(4+3)//we are using nodejs not browser so it raises error
+//code reacability should be high
+let name = "DIBYAJIT"
+let age = 24
+let isLoggedIn = false
+
+
+// number=>2 to power 53
+// bigint for large number
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefine => 
+// symbol => umique
+
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)//undefined
+
+
+console.log(typeof "DIBYA");
