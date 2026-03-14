1e47a2b61bd15d532443b528ac1093e6c895656a
dibyajitm300-cmd <dibyajitm300@gmail.com>
2026-03-14 06:01:42 +0000

    adding 1st file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_Id = 144553
+let accountEmail = "deevil90@gmal.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState ;
+
+// account_Id=9//NOT ALLOWED TO CONSTANTS
+
+accountEmail = "cv@jhujh"
+accountPassword="jbiued"
+accountCity="banglore"
+console.log(account_Id);
+console.table([account_Id,accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
